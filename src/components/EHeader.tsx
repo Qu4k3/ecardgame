dde7f41f659596a2c18f4cb6bb91d91aa3cf09b9
David Gracia <dagrala@gmail.com>
2023-04-26 18:07:52 +0200

    Clean basic styles
    Co-authored-by: Paul Guillamon <Qu4k3@users.noreply.github.com>

--- src/components/EHeader.tsx
+++ src/components/EHeader.tsx
@@ -1,8 +1,6 @@
-import styles from '@/styles/Home.module.css'
-
 export default function EHeader() {
   return (
-    <div className={styles.center}>
+    <div className="flex justify-center items-center">
         <h1 className=''>E-Card Game</h1>
         <h2 className=''>Ｅカード</h2>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 
-import styles from '@/styles/Home.module.css'
 import SiteLayout from '@/layouts/SiteLayout'
 import EMenu from '@/components/EMenu'
 
